454d9f61a8127f75f121e8ae6efd60bcba2d0f14
Kris Ferrario <kferrario@gmail.com>
2013-11-05 13:54:33 -0500

    Change navigation menu

--- views/_v_template.php
+++ views/_v_template.php
@@ -6,12 +6,27 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
 					
 	<!-- Controller Specific JS/CSS -->
+	<link rel="stylesheet" href="/css/sample-app.css" type="text/css">
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
 <body>	
-
+	<nav>
+		<menu>
+			<?php if($user): ?>
+				<li><a href="/posts/add">Tweet it</a></li>
+				<li><a href="/posts/"> See Tweets</a></li>
+				<li><a href="/posts/users"> Follow Tweeter</a></li>
+				<li><a href="/users/logout"> Get Out of Here</a></li>
+			<?php else: ?>
+				<li><a href="/users/signup">Sign Up</a></li>
+				<li><a href="/users/login">Login</a></lq>
+			<?php endif; ?>
+			
+			
+		</menu>
+	</nav>
 	<?php if(isset($content)) echo $content; ?>
 
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
